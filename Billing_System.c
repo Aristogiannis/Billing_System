4276dcf07482a043fc954d8ad27502f1ab229500
Aristogiannis <aristogiannisphilippis@gmail.com>
2023-01-25 13:40:58 +0200

    Working add (except for balance) working line counter

--- Billing_System.c
+++ Billing_System.c
@@ -4,45 +4,72 @@
 #include <stdlib.h>
 
 
-void option_add(){
- FILE *file;
- file = fopen("D:\\C\\accounts.txt","a");
- fprintf(file,"YO\n");
- fclose(file);
+FILE *file;
+const int length = 2000;
 
+/*===============================================*/
+
+void option_search(){
+    char c;
+    int lines = 0;
+      file = fopen("D:\\C\\accounts.txt","r");    
+    do{
+
+        c =fgetc(file);
+        if(c =='\n'){lines++;}
+
+    }while (c != EOF);
+ fclose(file);
+ printf("%d", lines);
 }
 
-void homepage(){
+/*===============================================*/
+void option_add(){ 
+ 
+ char fullname[100];
+ int due_date = 0;
+ double balance = 0;
+ 
+ printf("Name, Surname: ");
+ scanf("%s",&fullname);
+ printf("Due date (format = XXXXXX): ");
+ scanf("%d",&due_date);
+ printf("Balance: ");
+ scanf("%f",&balance);
+ 
+ file = fopen("D:\\C\\accounts.txt","a");
+ fprintf(file,"%s,%d,%f \n", fullname, due_date, balance);
+ fclose(file);
+}
+
+/*===============================================*/
+int homepage(){
     char title[] = "\n\n\t\tBilling System";
-    
-    
+    int answer;
     time_t t;  
     time(&t);
 
-    
     printf("%s %s %s \n\n", title,"\t\t\t",ctime(&t));
     printf("1: Add account\n2: Delete account\n3: Search account\n4: Exit\n");
     printf("  (Type from 1 to 4)\n");
+    scanf("%d",&answer);
+    return answer;
 }
 
+/*===============================================*/
 
 int main(){
- bool exitcode;
- exitcode = false;
- int answer;
- char stop; 
- char no[] ="nN";
-
- do{
- homepage();
- scanf("%d",&answer);
  
- switch (answer){
+ bool exitcode = false;
+
+ do{ 
+ switch (homepage()){
     case 1:
     option_add();
     break;
     
     case 2:
+    option_search();
     break;
     
     case 3:
@@ -58,4 +85,4 @@ int main(){
 
  return 0;
 }
-
+/*===============================================*/
